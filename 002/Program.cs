1de9933d0b4589053d823627e6106ed526a0c032
Dmitry-Murmansk <119297978+Dmitry-Murmansk@users.noreply.github.com>
2022-12-08 21:54:31 +0300

    задача3

---
+++ 002/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] numbers = new int[8];
+Console.Write("[");
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(0, 11);
+    Console.Write(" " + Method(i) + " ");
+}
+Console.Write("]");
+int Method(int a)
+{
+    return numbers[a];
+}
+//  Напишите программу, задает массив из [8]
+//  и выдает сумму их на экран.
